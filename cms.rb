400780bffede0d1fc37e05253d076c06e6675dd4
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-12-20 13:50:03 -0800

    Add functionality to display Markdown files and to edit document content

--- cms.rb
+++ cms.rb
@@ -1,13 +1,32 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "redcarpet"
 require "pry"
-
-root = File.expand_path("..", __FILE__)
 
 configure do
   enable :sessions
   set :session_secret, 'super_secret'
+end
+
+root = File.expand_path("..", __FILE__)
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  file_ext = File.extname(path)
+  content = File.read(path)
+
+  case file_ext
+  when '.txt'
+    headers["Content-Type"] = "text/plain"
+    content
+  when '.md'
+    render_markdown(content) 
+  end
 end
 
 get "/" do
@@ -22,10 +41,27 @@ get "/:filename" do
   file_path = root + "/data/" + params[:filename]
 
   if File.file?(file_path)
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+    load_file_content(file_path)
   else
     session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
   end
+end
+
+get "/:filename/edit" do
+  file_path = root + "/data/" + params[:filename]
+
+  @filename = params[:filename]
+  @content = File.read(file_path)
+  
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
 end
